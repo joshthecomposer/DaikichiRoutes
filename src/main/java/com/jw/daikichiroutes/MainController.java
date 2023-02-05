853549d26d64e2766513a6a5bf0d970eb2f302dc
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-04 23:44:05 -0500

    part 2

--- src/main/java/com/jw/daikichiroutes/MainController.java
+++ src/main/java/com/jw/daikichiroutes/MainController.java
@@ -1,5 +1,6 @@
 package com.jw.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,26 @@ public class MainController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow you will surely die!";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return "Congrats, you will travel to " + city + "!";
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") String number) {
+		
+		try {
+			int n = Integer.parseInt(number);
+			if (n % 2 == 0) {
+				return "Congrats, you're gonna live!";
+			} else {
+				return "Uh oh... I have bad news...";
+			}
+			
+		} catch (Exception e){
+			return "You did something wrong...";
+		}
 	}
 }
